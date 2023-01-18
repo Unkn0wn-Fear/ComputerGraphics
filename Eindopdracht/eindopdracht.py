30b066ce9a89087c596b00933ec9ba93688e303e
Marijn <93777269+Unkn0wn-Fear@users.noreply.github.com>
2023-01-18 21:48:49 +0100

    begin eindopdracht

---
+++ Eindopdracht/eindopdracht.py
@@ -0,0 +1,126 @@
+import math
+import random
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+
+x, y, z = 0.0, 0.0, 0.0
+vx, vy, vz = 0.003, 0.002, 0.001
+camera_x, camera_y, camera_z = 0, 0, 1
+
+
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+)
+
+
+def drawCube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+
+def keyboard(key, x, y):
+    global camera_x, camera_y, camera_z
+    if key == b'w':
+        camera_y += 0.1
+    elif key == b's':
+        camera_y -= 0.1
+    elif key == b'a':
+        camera_x -= 0.1
+    elif key == b'd':
+        camera_x += 0.1
+
+def drawSphere():
+    quad = gluNewQuadric()
+    glTranslatef(x, y, z)
+    gluSphere(quad, 0.1, 20, 20)
+
+def checkCollision():
+    global x, y, z, vx, vy, vz
+    print("x is = " + str(x))
+    print("y is = " + str(y))
+    print("z is = " + str(z))
+
+    rdn = (random.randint(0, 1))
+    rdf = float(rdn)
+    
+    if x < -1 or x > 1:
+        vx = -vx +  (-vx*(rdf))
+    if y < -1 or y > 1:
+        vy = -vy + (-vy*(rdf))
+    if z < -1 or z > 1:
+        vz = -vz + (-vz*(rdf))
+
+    x += vx + (vx*(rdf))
+    y += vy + (vy*(rdf))
+    z += vz + (vz(rdf))
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    # Move the camera
+
+
+    gluLookAt(camera_x, camera_y, camera_z, 1, 0, 0, 0, 1, 0)
+    # Draw the cube
+    drawCube()
+
+    # Draw the sphere
+    drawSphere()
+
+    checkCollision()
+
+    glutSwapBuffers()
+    glutPostRedisplay()
+
+
+glutInit()
+glutInitDisplayMode(GLUT_MULTISAMPLE | GLUT_DOUBLE | GLUT_DEPTH)
+glutInitWindowSize(900 , 900)
+glutCreateWindow("Bouncing sphere")
+glEnable(GL_LINE_SMOOTH)
+glEnable(GL_DEPTH_TEST)
+glMatrixMode(GL_PROJECTION)
+glFrustum(-1.333, 1.333, -1, 1, 1, 5)
+glMatrixMode(GL_MODELVIEW)
+glEnable(GL_LIGHTING)
+glEnable(GL_LIGHT0)
+glLight(GL_LIGHT0, GL_POSITION, [-3, 4, 5])
+glLight(GL_LIGHT0, GL_DIFFUSE, [0.5, 0.5, 0.5])
+glLight(GL_LIGHT0, GL_AMBIENT, [0.5, 0.5, 0.5])
+glLight(GL_LIGHT0, GL_SPECULAR, [1, 1, 1])
+glMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, [1, 1, 0, 1])
+glMaterial(GL_FRONT_AND_BACK, GL_SPECULAR, [1, 1, 1, 1])
+glMaterial(GL_FRONT_AND_BACK, GL_SHININESS, 40)
+glutDisplayFunc(display)
+glEnable(GL_DEPTH_TEST)
+glutKeyboardFunc(keyboard)    
+glutMainLoop()
--- Week4/projection.py
+++ Week4/projection.py
@@ -3,14 +3,16 @@ from OpenGL.GLU import *
 from OpenGL.GLUT import *
 
 class Lines:
-    def __init__(self, sizeX, sizeY):
+    def __init__(self, sizeX, sizeY,):
         self.sizeX = sizeX
         self.sizeY = sizeY
+     
         self.lines = []
 
     def addLine(self, p1, p2):
         x1, y1 = p1
         x2, y2 = p2
+        
         if 0 <= x1 < self.sizeX and \
            0 <= y1 < self.sizeY and \
            0 <= x2 < self.sizeX and \
@@ -21,10 +23,11 @@ class Lines:
         glClear(GL_COLOR_BUFFER_BIT)
         glColor(1, 1, 1)
         glBegin(GL_LINES)
-        for i in self.lines:
-            x1, y1, x2, y2 = i
-            glVertex(x1, y1)
-            glVertex(x2, y2)
+    
+        for ribben in ribben:
+            for punten in ribben:
+                glVertex3fv(punten[punten])
+        glEnd()
         glEnd()
         glFlush()
 
@@ -40,11 +43,46 @@ class Lines:
         glutDisplayFunc(self.display)
         glutKeyboardFunc(self.end)
         glutMainLoop()
+ 
+    def  vermenmatrixvector(self,matrix1,matrix2,matrix3,vector):
+        x1,y1,z1 = matrix1
+        x2,y2,z2 = matrix2
+        x3,y3,z3 = matrix3
+        
 
 # testcode
-lines = Lines(640, 480)
-lines.addLine((100, 100), (500, 300))
-p = (100, 200)
-for i in range(100, 600, 50):
-    lines.addLine(p, (i, 400))
+lines = Lines(10   ,10)
+
+punten = (
+    (-1,-1,-1), #0 v1
+    (1,-1,-1),  #1 V2
+    (1, 1,-1),  #2 V3
+    (-1, 1,-1), #3 V4
+    (-1,-1, 1), #4 V5
+    (1,-1, 1),  #5 V6
+    (1, 1, 1),  #6 V7
+    (-1, 1, 1)  #7 V8
+
+)
+ribben = (
+    (0,1),
+    (1,2),
+    (2,3),
+    (3,0),
+    (0,4),
+    (1,5),
+    (2,6),
+    (3,7),
+    (4,5),
+    (5,6),
+    (6,7),
+    (7,4),
+
+
+
+)
+# lines.addLine((-5, -5), (6, 6))
+# p = (100, 200)
+# for i in range(100, 600, 50):
+#     lines.addLine(p, (i, 400))
 lines.draw()
